491e4dc67137321f5ef8e1a93eae1f9935ca1ce5
Coohx <huangxin2028@gmail.com>
2018-04-27 22:07:00 +0800

    update

--- venv/python基础/dict.py
+++ venv/python基础/dict.py
@@ -13,7 +13,7 @@ print(m)
 m["name"] = "cc"
 print(m)
 
-name = [1, 2, 3, 4, 5, 6, ]
+name = [1, 2, 3, 4, 5, 6,]
 
 person = {"name": "merlin",
           "gender": "man",
@@ -24,5 +24,8 @@ for i in enumerate(person):
     """取对象对应的索引和值"""
     print(i)
 
+
+for index, value in enumerate(name, 2):
+    print("{0}: {1}".format(index, value))
 # 导入
 # alt + enter   快速导入要使用的模块
