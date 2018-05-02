754579e051d0620f6f03e26e7f3e16cf52e1e9ea
Coohx <huangxin2028@gmail.com>
2018-05-02 20:04:55 +0800

    update

---
+++ venv/python_package/daemon.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time   : 2018/4/24 21:16
+# @Author : merlinhuang
+# @File   : daemon'.py
+
+
+
+import os, random
+
+
+print(os.name)
+
+print(os.linesep)
+
+for i in range(0, 1000):
+    print(random.randint(1, 6))
+
+
