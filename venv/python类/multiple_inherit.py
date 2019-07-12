2a9fc507c0b91b552baa6eb7b8f7aa8c1681fabb
Coohx <huangxin2028@gmail.com>
2019-07-12 21:17:41 +0800

    class super()

--- venv/python类/multiple_inherit.py
+++ venv/python类/multiple_inherit.py
@@ -110,3 +110,35 @@ class Talent(Speaker, Student):
 print(Talent.__mro__)
 talent1 = Talent("Tim", 25, 60, 4, "Python")
 talent1.speak()  # 优先继承括号中排前地父类的方法
+talent2 = Talent("tom", 19, 70, 9, 'C++')
+talent2.speak()
+
+# 可以对实例变量动态绑定属性/方法,称为该实例的特有属性或方法
+talent1.addr = "china"
+print(talent1.addr)
+
+
+# super()函数
+class A:
+    """base class"""
+    def __init__(self, name):
+        self.name = name
+        print("父类的__init__方法被执行了!")
+
+    def show(self):
+        print("父类的show方法被执行了!")
+
+
+class B(A):
+    """sub class"""
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+
+    def show(self):
+        # 利用super()函数强制调用父类同名的方法,不覆盖父类同名方法
+        super().show()
+
+
+obj = B("M", 15)
+obj.show()
