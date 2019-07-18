7ca02f2b311aeee17a8c7affb6b86beb182b4814
Coohx <huangxin2028@gmail.com>
2019-07-18 10:20:25 +0800

    PEP8 repair

--- venv/python类/class_encapsulation.py
+++ venv/python类/class_encapsulation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 class ProtectMe:
     """封装test"""
     def __init__(self):
@@ -19,6 +20,6 @@ class ProtectMe:
 if __name__ == "__main__":
     p = ProtectMe()
     print(p.me)
-    #print(p.__name)  # 类内部私有方法，不允许外部调用或修改
+    # print(p.__name)  # 类内部私有方法，不允许外部调用或修改
     p.code()  # 类自己code()方法调用了私有方法__python()，对外部不可见
 
