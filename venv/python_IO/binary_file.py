ff610f449a67077bc35604e145dd2a07ffcec262
Coohx <huangxin2028@gmail.com>
2019-12-22 20:46:47 +0800

    py 文件读取

---
+++ venv/python_IO/binary_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+
+"""
+
+with open("test.jpg", "rb") as f:
+    print(f.read())
--- venv/python_IO/truncate.py
+++ venv/python_IO/truncate.py
@@ -5,19 +5,21 @@
 """
 文件对象的 truncate([size]) 方法用于截断文件
   fileObject.truncate( [ size ])
-    如果指定了可选参数 size，则表示截断文件为 size 个字符 , 截断之后 size 后面的所有字符被删除
-    如果没有指定 size，则从当前位置起截断
+    如果指定了可选参数 size，则表示从文件开头截断文件为 size 个字符 , 截断之后 size 后面的所有字符被删除
+    如果没有指定 size，从文件开头截断到光标当前位置，剩余内容删除.
 """
 
 
-with open("voip.log", "r+") as fo:
+with open("voip.log", "r+", encoding='utf-8') as fo:
     print(fo.name)
-    line = fo.read()
-    print("读取第一行: %s" % line)
-    # 截断剩下字符串
+    fo.seek(28)  # 将光标移动到第二行末尾
     print(fo.tell())
-    # fo.truncate(5)  # 有size,从首行首字节开始截断
-    fo.truncate()  # 当前位置为文件末尾，相当于不截断
-    line = fo.readline()
-    print("读取截断后数据: %s"  % line)
-
+    fo.truncate()  # size为空，从文件开头截断到光标当前位置，剩余内容删除
+    fo.seek(0, 0)  # 移动到最开头
+    line = fo.readlines()  # 从当前光标位置下一个字节开始读取
+    print("读取截断后数据: %s" % line)
+    print(fo.tell())  # 此时光标位于文件末尾
+    fo.truncate(10)  # 指定size, 从文件开头开始截断为size个字符
+    fo.seek(0, 0)  # 光标置于文件最开始，进行读取
+    content = fo.read()
+    print("截取10个字符10后：%s" % content)
