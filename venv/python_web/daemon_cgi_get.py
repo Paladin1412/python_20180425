799570e538c6c6c4742846502902372da9849ddb
Coohx <huangxin2028@gmail.com>
2020-03-20 21:04:58 +0800

    cgi get

---
+++ venv/python_web/daemon_cgi_get.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+python cgi get方法处理
+url实例，get方法
+"""
+
+
+import cgi
+#import cgitb
+import sys
+import codecs
+sys.stdout = codecs.getwriter('utf8')(sys.stdout.buffer)
+
+
+# 创建 FieldStorage 的实例化
+form = cgi.FieldStorage()
+
+# 获取cgi数据
+site_name = form.getvalue('name')
+site_url = form.getvalue('url')
+
+print("Content-type: text/html")
+print("")
+print("""
+      <html>
+      <head>
+      <meta charset="utf-8">
+      <title>一个CGI get测试实例</title>
+      </head>
+      <body>
+     """)
+print(r"<h2>%s业务：%s<h2/>" % (site_name, site_url))
+print("""
+      <body/>
+      <html/>
+      """)
